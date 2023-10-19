e6e253b5192a3e238494090e32ddf69d75b1eab1
Milton Mazzarri <me@milmazz.uno>
2023-10-18 21:54:01 -0500

    initial deps

--- mix.exs
+++ mix.exs
@@ -1,12 +1,15 @@
 defmodule Bier.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :bier,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps()
     ]
   end
@@ -19,11 +22,18 @@ defmodule Bier.MixProject do
     ]
   end
 
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:bandit, "~> 1.0-pre"},
+      {:ex_doc, "~> 0.25", only: :dev, runtime: false},
+      {:jason, "~> 1.2"},
+      {:nimble_options, "~> 0.3"},
+      {:nimble_parsec, "~> 1.2"},
+      {:plug, "~> 1.4"}
     ]
   end
 end
